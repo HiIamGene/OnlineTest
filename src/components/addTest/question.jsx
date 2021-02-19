e31cd2003bc07c9de8e4806cca8fb81236b2ff36
HiIamGene <l.jiraphat@gmail.com>
2021-02-19 13:19:41 +0700

    refactor

--- src/components/addTest/question.jsx
+++ src/components/addTest/question.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { Input, Row, Col, Button } from 'antd';
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { Link } from 'react-router-dom';
@@ -6,9 +6,9 @@ import { v4 as uuid } from "uuid";
 
 const itemsFromBackend = [
   { id: uuid(), name: "สีอะไรคือสีโทนร้อน?" }
-  , { id:uuid(), name: "ทำไมต้องทำ Usability Testing ?" }
-  , { id:uuid(), name: "Low-fidelity prototype คือ ?" }
-  , { id:uuid(), name: "Hi-fidelity prototype  คือ ?" }]
+  , { id: uuid(), name: "ทำไมต้องทำ Usability Testing ?" }
+  , { id: uuid(), name: "Low-fidelity prototype คือ ?" }
+  , { id: uuid(), name: "Hi-fidelity prototype  คือ ?" }]
 const columnsFromBackend = {
   [uuid()]: {
     name: "การออกแบบUI",
@@ -18,69 +18,59 @@ const columnsFromBackend = {
     name: "สีกับความรู้สึก",
     items: []
   }
-  
+
 };
-const onDragEnd = (result, columns, setColumns) => {
+const newForm = {
+  [uuid()]: {
+    name: "",
+    items: []
+  },
+
+};
+const onDragEnd = (result, selectColumn, setSelectColumn) => {
   if (!result.destination) return;
   const { source, destination } = result;
+  /*const copiedItems = [...selectColumn.items];
+  const [removed] = copiedItems.splice(source.index, 1);
+  copiedItems.splice(destination.index, 0, removed);
+  setSelectColumn({
+    [source.droppableId]: {
+      ...selectColumn,
+      items: copiedItems
+    }
+  });*/
 
-  if (source.droppableId !== destination.droppableId) {
-    const sourceColumn = columns[source.droppableId];
-    const destColumn = columns[destination.droppableId];
-    const sourceItems = [...sourceColumn.items];
-    const destItems = [...destColumn.items];
-    const [removed] = sourceItems.splice(source.index, 1);
-    destItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...sourceColumn,
-        items: sourceItems
-      },
-      [destination.droppableId]: {
-        ...destColumn,
-        items: destItems
-      }
-    });
-  } else {
-    const column = columns[source.droppableId];
-    const copiedItems = [...column.items];
-    const [removed] = copiedItems.splice(source.index, 1);
-    copiedItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...column,
-        items: copiedItems
-      }
-    });
-  }
 };
 function Question(props) {
-  const [columns, setColumns] = useState([]);
+  const [columns, setColumns] = useState(columnsFromBackend);
+  const [selectColumn, setSelectColumn] = useState([]);
   useEffect(() => {
-    let temp = columnsFromBackend
-    console.log(temp)
-    for (const [columnId, column] of Object.entries(columnsFromBackend)) {
-      if(column.name===props.groupName){
+    let temp = []
+    for (const [columnId, column] of Object.entries(columns)) {
+      if (column.name === props.groupName) {
+        setSelectColumn(column.items)
       }
-      else{
-        delete temp[columnId]
+      else {
+        //delete temp[columnId]
       }
     }
-    setColumns({ ...temp })
+    /*if (!temp) {
+      temp = newForm
+      setSelectColumn({ ...temp })
+    }*/
+
   }, [props.groupName]);
-  const onClickAddColumn = (e) => {
-    let temp = columns
-    temp[e].items.push({ id: uuid(), name: "Please enter question" })
-    setColumns({ ...temp })
+  const onClickAddColumn = () => {
+    let temp = selectColumn
+    temp.push({ id: uuid(), name: "Please enter question" })
+    setSelectColumn([...temp ])
 
   }
-  const onClickdeletColumn = (e,column) => {
-    let temp = columns
+  const onClickdeletColumn = (index) => {
+    let temp = selectColumn
     //let select= temp[e].items[column]
-    temp[e].items.splice(column,1)
-    setColumns({ ...temp })
+    temp.splice(index, 1)
+    setSelectColumn([...temp ])
 
   }
 
@@ -92,30 +82,27 @@ function Question(props) {
       </Col>
       <Col span={24} >
         <div style={{ display: "flex", justifyContent: "flex-end" }}>
-          <Button onClick={() => { onClickAddColumn("31ded736-4076-4b1c-b38f-7e8d9fa78b41") }} type="primary" size="large" style={{ alignItems: "center", background: '#F43A09' }}>Add Header</Button>
+          <Button onClick={() => { onClickAddColumn() }} type="primary" size="large" style={{ alignItems: "center", background: '#F43A09' }}>Add Question</Button>
         </div>
         <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
           <DragDropContext
-            onDragEnd={result => onDragEnd(result, columns, setColumns)}
+            onDragEnd={result => onDragEnd(result, selectColumn, setSelectColumn)}
           >
-            {Object.entries(columns).map(([columnId, column], index) => {
-              return (
                 <div
                   style={{
                     display: "flex",
                     flexDirection: "column",
                     alignItems: "center"
                   }}
-                  key={columnId}
                 >
                   <div style={{ margin: 8 }}>
-                    <Droppable droppableId={columnId} key={columnId}>
+                    <Droppable >
                       {(provided, snapshot) => {
                         return (
                           <div
                             {...provided.droppableProps} ref={provided.innerRef} style={{ background: snapshot.isDraggingOver ? "lightblue" : "lightgrey", padding: 4, minWidth: 1470, minHeight: 50, display: "flex", flexDirection: "column", alignItems: "center" }}
                           >
-                            {column.items.map((item, index) => {
+                            {selectColumn.map((item, index) => {
                               return (
                                 <Draggable
                                   key={item.id}
@@ -143,12 +130,12 @@ function Question(props) {
                                         <div>
 
                                           <Row>
-                                            <div style={{ fontSize: 30, fontWeight: "bold", display: "block", color: "#000000" }} >{index+1}.
-                                          <Button onClick={()=>props.onSelectquestionName(columns[columnId].items[index].name,index+1)} type="primary" htmlType="submit" className="login-form-button" style={{ fontSize: 30, background: '#F4A940', color: '#FFFFFF', width: 1330, height: 100, marginTop: 30, textAlign: 'left' }}>{item.name}</Button>
+                                            <div style={{ fontSize: 30, fontWeight: "bold", display: "block", color: "#000000" }} >{index + 1}.
+                                          <Button onClick={() => props.onSelectquestionName(selectColumn[index].name, index + 1)} type="primary" htmlType="submit" className="login-form-button" style={{ fontSize: 30, background: '#F4A940', color: '#FFFFFF', width: 1330, height: 100, marginTop: 30, textAlign: 'left' }}>{item.name}</Button>
                                             </div>
                                             <table style={{ marginTop: 30, marginLeft: 20 }}>
                                               <div style={{ marginTop: 40, marginLeft: 10, fontSize: 30 }}>
-                                                <Button onClick={() => onClickdeletColumn(columnId, index)} type="primary" shape="circle" size="large" style={{ background: '#F4A940', color: '#FFFFFF' }}>x</Button>
+                                                <Button onClick={() => onClickdeletColumn(index)} type="primary" shape="circle" size="large" style={{ background: '#F4A940', color: '#FFFFFF' }}>x</Button>
                                               </div>
                                             </table>
                                           </Row>
@@ -166,8 +153,6 @@ function Question(props) {
                     </Droppable>
                   </div>
                 </div>
-              );
-            })}
           </DragDropContext>
         </div>
       </Col>
